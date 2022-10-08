f2b1f382a62a7dc00e6023d5ece1896d8e10c6bb
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-10-08 19:48:44 +0200

    feat: create `<NavBar>` component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
+import NavBar from "./components/navbar";
 import Counters from "./components/counters";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+            <React.Fragment>
+                <NavBar />
+                <main className="container">
                     <Counters />
-                </header>
-            </div>
+                </main>
+            </React.Fragment>
         );
     }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 it("renders without crashing", () => {
     render(<App />);
-});
-
-test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
 });
