4dfdc60b8130147134b78228cf4108912ea12520
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-10-08 20:40:57 +0200

    chore: Use Stateless Functional Component for `<NavBar>`

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <span className="navbar-brand mb-0 h1">
-                    Navbar
-                    {this.props.badge !== undefined && (
-                        <span className="badge badge-pill badge-secondary m-2">
-                            {this.props.badge}
-                        </span>
-                    )}
-                </span>
-            </nav>
-        );
-    }
-}
+const NavBar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">
+                Navbar
+                {props.badge !== undefined && (
+                    <span className="badge badge-pill badge-secondary m-2">
+                        {props.badge}
+                    </span>
+                )}
+            </span>
+        </nav>
+    );
+};
 
 export default NavBar;
